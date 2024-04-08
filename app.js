50a21bea05fc44405cb7253078049d1f1005fd0c
Chang Liu <liu.chang31@northeastern.edu>
2024-04-07 18:44:17 -0700

    update missing packages

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
-import Lab5 from "./lab5.js";
-import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./modules/routes.js";
-import AssignmentRoutes from "./assignments/routes.js";
+// import Lab5 from "./lab5.js";
+// import CourseRoutes from "./courses/routes.js";
+// import ModuleRoutes from "./modules/routes.js";
+// import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
@@ -38,8 +38,7 @@ app.use(session(sessionOptions));
 
 app.use(express.json());
 UserRoutes(app);
-AssignmentRoutes(app);
-ModuleRoutes(app);
-CourseRoutes(app);
-Lab5(app);
+// AssignmentRoutes(app);
+// ModuleRoutes(app);
+// CourseRoutes(app);
 app.listen(process.env.PORT || 4000);
