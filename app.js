a704509133332a26c92efcc18770194ec24eae77
Chang Liu <liu.chang31@northeastern.edu>
2024-05-27 22:05:20 -0700

    update cors

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: process.env.FRONTEND_URL
+        // origin: process.env.FRONTEND_URL
+        origin: 'https://physgenius.netlify.app',
     })
 );
 
